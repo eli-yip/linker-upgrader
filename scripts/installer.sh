ed2bcad9b27d4d65014d7ec4f885559601bc0a40
Su Yang <soulteary@gmail.com>
2025-06-11 17:55:24 +0800

    chore: update installer

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -2,8 +2,8 @@
 
 mkdir linker-upgrade
 cd linker-upgrade/
-curl -LO https://github.com/linker-bot/linker-upgrader/releases/download/v0.0.2/linker-upgrader_0.0.2_linux_arm64.tar.gz
-tar zxvf linker-upgrader_0.0.2_linux_arm64.tar.gz
+curl -LO https://github.com/linker-bot/linker-upgrader/releases/download/v0.0.3/linker-upgrader_0.0.3_linux_arm64.tar.gz
+tar zxvf linker-upgrader_0.0.3_linux_arm64.tar.gz
 
 sudo curl -L https://raw.githubusercontent.com/linker-bot/linker-upgrader/refs/heads/main/scripts/etc/systemd/system/linker-upgrade.service -o /etc/systemd/system/linker-upgrade.service
 sudo mkdir -p /etc/linker-upgrader/
